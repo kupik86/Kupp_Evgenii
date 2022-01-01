cb809134709a1e4209792b448a58ca42bc0864a3
Kupik86 <e.kupp@yandex.ru>
2022-01-01 15:31:59 +0300

    решение 2 задачи ДЗ №4

--- Kupp_Evgeniy_dz_4/task_2.py
+++ Kupp_Evgeniy_dz_4/task_2.py
@@ -11,3 +11,27 @@
 # от того, в каком регистре был передан аргумент? В качестве примера выведите курсы доллара и евро.
 
 # Решение
+import requests
+
+
+def currency_rates(item):
+    url = 'http://www.cbr.ru/scripts/XML_daily.asp'
+    respone = requests.get(url)
+    # определяем кодировку страницы и декодируем ее, приводя содержимое к строковому типу
+    content = respone.content.decode(encoding=respone.encoding)
+    print(content)
+    rezult = None
+    if item not in content:
+        return rezult
+    else:
+        el_1 = content.split(f'{item}')[1]  # отрезаем от строки все лишнее до искомой валюты
+        el_2 = el_1.split('</Value>')[0]  # отрезаем от строки все лишнее после стоимости искомой валюты
+        # вычленяем из строки стоимость приводим ее к числовому типу данных и округляем до 2 разрядов
+        rezult = round(float(el_2.split('<Value>')[1].replace(',', '.')), 2)
+        return f'Курс валюты {rezult} руб.'
+
+
+if __name__ == '__main__':
+    money = input('Введите валюту: ')
+    print(currency_rates(money))
+
