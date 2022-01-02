6fdf8b1b12aafa8eec35d18c66c1c5fb8dc8fb93
Kupik86 <e.kupp@yandex.ru>
2022-01-02 17:58:52 +0300

    Обновление решения 5 задачи ДЗ №4

--- Kupp_Evgeniy_dz_4/task_5.py
+++ Kupp_Evgeniy_dz_4/task_5.py
@@ -2,5 +2,13 @@
 
 # Задача №5
 # * (вместо 4) Доработать скрипт из предыдущего задания: теперь он должен работать и из консоли. Например:
+# > python task_4_5.py USD
+# 75.18, 2020-09-05
 
 # Решение
+import sys
+import task_3
+
+
+# cansole = sys.argv
+print(task_3.currency_rates(sys.argv[1]))
