42fc5b3c411283cec87d2673fc5c94f9e4f74d9d
Kupik86 <e.kupp@yandex.ru>
2022-01-04 17:07:14 +0300

    Решение задачи 5 ДЗ №3

--- Kupp_Evgeniy_dz_3/task_5.py
+++ Kupp_Evgeniy_dz_3/task_5.py
@@ -11,4 +11,22 @@
 # ["лес завтра зеленый", "город вчера веселый"]
 
 # Решение
+import random
 
+
+def get_jokes(y):
+    jokes = []
+    for i in range(y):
+        el_n = random.choice(nouns)
+        el_av = random.choice(adverbs)
+        el_aj = random.choice(adjectives)
+        sequence = f'{el_n} {el_av} {el_aj}'
+        jokes.append(sequence)
+    print(jokes)
+
+
+nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
+adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
+adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
+
+get_jokes(int(input('введите количество шуток: ')))
