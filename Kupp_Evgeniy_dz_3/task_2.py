9a5d56f8566a1edc444dd91b4f1642657c20cb93
Kupik86 <e.kupp@yandex.ru>
2022-01-04 11:08:01 +0300

    Решение задачи 2 ДЗ №3

--- Kupp_Evgeniy_dz_3/task_2.py
+++ Kupp_Evgeniy_dz_3/task_2.py
@@ -9,3 +9,30 @@
 # "два"
 
 # Решение
+def num_translate(x):
+    numbers_en = {
+        'one': 'один',
+        'two': 'два',
+        'three': 'три',
+        'four': 'четыре',
+        'five': 'пять',
+        'six': 'шесть',
+        'seven': 'семь',
+        'eight': 'восемь',
+        'nine': 'девять',
+        'zero': 'ноль',
+        'ten': 'десять'
+    }
+    return numbers_en.get(x)
+
+
+en = input('Введите число на английском языке: ')
+if en.istitle():  # Определяем с заглавной буквы ввод или нет. Если да, то...
+    en = en.lower()
+    # если ввести с заглавной значение, которого нет в словаре, идет None. Создаем условие чтобы ее обработать
+    if num_translate(en):
+        print(num_translate(en).capitalize())
+    else:
+        print(num_translate(en))
+else:  # Если не с заглавной буквы ввод, тогда ...
+    print(num_translate(en))
