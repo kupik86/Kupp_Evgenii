f11307ab13a64ef937cedd604c0fc67d6946a519
Kupik86 <e.kupp@yandex.ru>
2022-01-11 22:26:59 +0300

    решение задачи 1 ДЗ №5

--- Kupp_Evgeniy_dz_5/task_1.py
+++ Kupp_Evgeniy_dz_5/task_1.py
@@ -15,3 +15,13 @@
 # ...StopIteration...
 
 # Решение
+def odd_nums(n):
+    for i in range(1, n+1):
+        if i % 2 != 0:
+            yield i
+
+
+odd_to_15 = odd_nums(15)
+
+print(next(odd_to_15))
+print(next(odd_to_15))
