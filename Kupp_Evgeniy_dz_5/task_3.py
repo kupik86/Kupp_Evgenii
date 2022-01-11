5fea41085fc7cb85413a5e5bff533f689338ceb5
Kupik86 <e.kupp@yandex.ru>
2022-01-11 22:27:18 +0300

    решение задачи 3 ДЗ №5

--- Kupp_Evgeniy_dz_5/task_3.py
+++ Kupp_Evgeniy_dz_5/task_3.py
@@ -21,3 +21,25 @@
 # генератор даст эффект.
 
 # Решение
+from itertools import zip_longest
+
+
+tutors = [
+    'Иван', 'Анастасия', 'Петр', 'Сергей',
+    'Дмитрий', 'Борис', 'Елена', 'Степан', 'Михаил'
+]
+klasses = [
+    '9А', '7В', '9Б', '9В', '8Б', '10А', '10Б', '9А'
+]
+gen = (el for el in zip_longest(tutors, klasses[:len(tutors)]))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+
