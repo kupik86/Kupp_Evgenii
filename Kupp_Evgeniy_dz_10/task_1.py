5240cdd0193fb51e421428273601f46f02d2db41
Kupik86 <e.kupp@yandex.ru>
2022-01-30 21:23:42 +0300

    решение задачи 1 ДЗ 10

--- Kupp_Evgeniy_dz_10/task_1.py
+++ Kupp_Evgeniy_dz_10/task_1.py
@@ -23,3 +23,28 @@
 # с первым элементом первой строки второй матрицы и пр.
 
 # Решение
+class Matrix:
+    def __init__(self, *args):
+        self.lists = []
+        self.lists_int = [*args]
+        for i in args:
+            self.lists.append(str(i))
+
+    def __str__(self):
+        return '\n'.join(self.lists)
+
+    def __add__(self, other):
+        r = []
+        for i in zip(self.lists_int, other.lists_int):
+            c = []
+            for h in range(len(self.lists_int[0])):
+                g = (i[0][h] + i[1][h])
+                c.append(g)
+            r.append(str(c))
+        return Matrix('\n'.join(r))
+
+
+obj_1 = Matrix([1, 2, 3, 2], [4, 5, 6, 5], [8, 1, 3, 1], [8, 1, 3, 4])
+obj_2 = Matrix([3, 2, 1, 1], [6, 5, 4, 4], [6, 8, 4, 4], [8, 1, 3, 1])
+print(f'{obj_1}\n\n{obj_2}')
+print(f'\n{obj_1 + obj_2}')
