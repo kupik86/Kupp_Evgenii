6fc0c506908c7c8ce19141fc957e771a23dee635
Kupik86 <e.kupp@yandex.ru>
2022-02-05 17:26:17 +0300

    решение 5 задачи ДЗ № 9

--- Kupp_Evgeniy_dz_9/task_5.py
+++ Kupp_Evgeniy_dz_9/task_5.py
@@ -10,3 +10,40 @@
 # создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
 
 # Решение
+class Stationery:
+    title = 'Канцелярская принадлежность'
+
+    def draw(self):
+        return f'Запуск отрисовки'
+
+
+class Pen(Stationery):
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        return 'не пишет, срержень закончился'
+
+
+class Pencil(Stationery):
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        return '(перед использованием) нужно заточить'
+
+
+class Handle(Stationery):
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        return 'на вкус и цвет у каждого разные'
+
+
+pen = Pen('Ручка гелевая')
+print(pen.title, pen.draw())
+pencil = Pencil('Карандаш')
+print(pencil.title, pencil.draw())
+handle = Handle('Фломастеры')
+print(handle.title, handle.draw())
