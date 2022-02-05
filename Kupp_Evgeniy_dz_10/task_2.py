17b94a4e42f6ee53f43d94c24ba2c262d300ea68
Kupik86 <e.kupp@yandex.ru>
2022-02-05 23:29:05 +0300

    решение 2 задачи ДЗ № 10

--- Kupp_Evgeniy_dz_10/task_2.py
+++ Kupp_Evgeniy_dz_10/task_2.py
@@ -12,3 +12,38 @@
 # классы для основных классов проекта и проверить работу декоратора @property.
 
 # Решение
+class Coat:
+    def __init__(self, title, size):
+        self.title = title
+        self.size = size
+        Clothes.sum_cloth_lst.append(round((self.size / 6.5 + 0.5), 1))
+
+    def __str__(self):
+        return f'{round((self.size / 6.5 + 0.5), 1)} м ткани уйдет чтобы сшить {self.title}'
+
+
+class Suit:
+    def __init__(self, title, size):
+        self.title = title
+        self.size = size
+        Clothes.sum_cloth_lst.append(round((self.size * 2 + 0.3), 1))
+
+    def __str__(self):
+        return f'{round((self.size * 2 + 0.3), 1)} м ткани уйдет чтобы сшить {self.title}'
+
+
+class Clothes(Coat, Suit):
+    sum_cloth_lst = []
+
+    @staticmethod
+    def sum_cloth():
+        return f'Всего потребуется ткани {sum(Clothes.sum_cloth_lst)} м'
+
+
+suit_1 = Suit('пиджак', 1.7)
+suit_2 = Suit('фрак', 1.9)
+coat_1 = Coat('зимнее пальто', 52)
+print(suit_1)
+print(suit_2)
+print(coat_1)
+print(Clothes.sum_cloth())
