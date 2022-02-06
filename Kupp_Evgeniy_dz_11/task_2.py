1f339c427d937dfdf6eb34d47cd46a5cc4c21bb0
Kupik86 <e.kupp@yandex.ru>
2022-02-06 17:58:11 +0300

    решение 2 задачи ДЗ № 11

--- Kupp_Evgeniy_dz_11/task_2.py
+++ Kupp_Evgeniy_dz_11/task_2.py
@@ -7,3 +7,34 @@
 # завершиться с ошибкой.
 
 # Решение
+class MyErr(Exception):
+    pass
+
+
+class MyClass:
+    def __init__(self, number_1, number_2):
+        self.number_1 = int(number_1)
+        self.number_2 = int(number_2)
+        self.res = self.calculation(number_1, number_2)
+
+    @staticmethod
+    def calculation(number_1, number_2):
+        try:
+            if number_2 == 0:
+                raise MyErr(f'на {number_2} делить нельзя')
+        except MyErr as err:
+            return err
+        else:
+            res = int(number_1 / number_2)
+            return res
+
+    def __str__(self):
+        return f'{self.res}'
+
+
+my_1 = MyClass(12, 2)
+print(my_1)
+my_2 = MyClass(12, 0)
+print(my_2)
+my_3 = MyClass(0, 45)
+print(my_3)
