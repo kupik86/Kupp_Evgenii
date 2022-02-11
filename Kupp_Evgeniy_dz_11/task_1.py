936c5724c085b4a986ca7e925deec6df79bf783d
Kupik86 <e.kupp@yandex.ru>
2022-02-11 16:05:52 +0300

    решение 1 задачи ДЗ № 11

--- Kupp_Evgeniy_dz_11/task_1.py
+++ Kupp_Evgeniy_dz_11/task_1.py
@@ -18,38 +18,33 @@ class MyError2(Exception):
 
 class Date:
     def __init__(self, date):
-        self.date = Date.from_date_to_numbers(date)
+        self.date = list(map(int, date.split('-')))
 
     @staticmethod
     def validate(date):
         try:
-            if not (1 <= date['day'] <= 31):
-                raise MyError1(f'{date["day"]} - измените день')
+            if not (1 <= date[0] <= 31):
+                raise MyError1(f'{date[0]} - измените день')
         except MyError1 as err1:
             print(err1)
         else:
             try:
-                if not (1 <= date["month"] <= 12):
-                    raise MyError2(f'{date["month"]} - измените месяц')
+                if not (1 <= date[1] <= 12):
+                    raise MyError2(f'{date[1]} - измените месяц')
             except MyError2:
                 return False
             else:
                 return True
         finally:
             try:
-                if not (1 <= date["month"] <= 12):
-                    raise MyError2(f'{date["month"]} - измените месяц')
+                if not (1 <= date[1] <= 12):
+                    raise MyError2(f'{date[1]} - измените месяц')
             except MyError2 as err2:
                 print(err2)
 
-    @classmethod
-    def from_date_to_numbers(cls, date):
-        day, month, year = map(int, date.split('-'))
-        return {'day': day, 'month': month, 'year': year}
-
     def __str__(self):
         if self.validate(self.date):
-            return f'{self.date["day"]}.{self.date["month"]}.{self.date["year"]}'
+            return f'{self.date[0]}.{self.date[1]}.{self.date[2]}'
         else:
             return f'неверный формат даты'
 
