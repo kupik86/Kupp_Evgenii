856e32719efb033386a30e4045c7cba2e78fba6a
Kupik86 <e.kupp@yandex.ru>
2022-02-11 16:06:11 +0300

    решение 4 задачи ДЗ № 11

--- Kupp_Evgeniy_dz_11/task_4.py
+++ Kupp_Evgeniy_dz_11/task_4.py
@@ -8,3 +8,23 @@
 # уникальные для каждого типа оргтехники.
 #
 # Решение
+class WareHouse:
+    position = 'Москва'
+
+
+class OfficeEquipment:
+    paper = 'A4'
+    color = 'white'
+    voltage = 220
+
+
+class Scanner(OfficeEquipment):
+    scan_file = True
+
+
+class Printer(OfficeEquipment):
+    print_file = True
+
+
+class Xerox(OfficeEquipment):
+    copy_file = True
